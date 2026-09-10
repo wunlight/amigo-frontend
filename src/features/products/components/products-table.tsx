466c56d2cba875b0da9b1b09645d48cb3dfa556f
wunlight <gluigi132@gmail.com>
2026-09-10 15:20:56 +0700

    wip: adjusted products table ui

--- src/features/products/components/products-table.tsx
+++ src/features/products/components/products-table.tsx
@@ -17,59 +17,6 @@ type ProductsTableProps = {
   refetch: () => void;
 };
 
-function ProductTableBody({
-  products,
-  isLoading,
-  isError,
-}: ProductsTableProps) {
-  if (isLoading) {
-    return (
-      <TableRow>
-        <TableCell colSpan={2} className="h-24 text-center">
-          Loading products...
-        </TableCell>
-      </TableRow>
-    );
-  }
-
-  if (isError) {
-    return (
-      <TableRow>
-        <TableCell colSpan={2} className="h-24 text-center">
-          Failed to load products.
-        </TableCell>
-      </TableRow>
-    );
-  }
-
-  if (products.length === 0) {
-    return (
-      <TableRow>
-        <TableCell colSpan={2} className="h-24 text-center">
-          No products found.
-        </TableCell>
-      </TableRow>
-    );
-  }
-
-  return products.map((product) => (
-    <TableRow key={product.id}>
-      <TableCell>{product.name}</TableCell>
-      <TableCell>
-        <div className="flex justify-end gap-3">
-          <Button variant="secondary" size="icon">
-            <span className="icon-[hugeicons--pencil-edit-02]" />
-          </Button>
-
-          <Button variant="destructive" size="icon">
-            <span className="icon-[hugeicons--delete-02]" />
-          </Button>
-        </div>
-      </TableCell>
-    </TableRow>
-  ));
-}
-
 function ProductsTable({
   products,
   isLoading,
@@ -83,22 +30,55 @@ function ProductsTable({
           <TableHead>Name</TableHead>
           <TableHead>
             <div className="flex justify-end">
-              <Button>
-                <span className="icon-[hugeicons--plus]" />
-                <span>Add Product</span>
-              </Button>
+              <span>Actions</span>
             </div>
           </TableHead>
         </TableRow>
       </TableHeader>
 
       <TableBody>
-        <ProductTableBody
-          products={products}
-          isLoading={isLoading}
-          isError={isError}
-          refetch={() => refetch()}
-        />
+        {isLoading && (
+          <TableRow>
+            <TableCell colSpan={2} className="h-24 text-center">
+              Loading products...
+            </TableCell>
+          </TableRow>
+        )}
+
+        {!isLoading && isError && (
+          <TableRow>
+            <TableCell colSpan={2} className="h-24 text-center">
+              Failed to load products.
+            </TableCell>
+          </TableRow>
+        )}
+
+        {!isLoading && !isError && products.length === 0 && (
+          <TableRow>
+            <TableCell colSpan={2} className="h-24 text-center">
+              No products found.
+            </TableCell>
+          </TableRow>
+        )}
+
+        {!isLoading &&
+          !isError &&
+          products.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>
+                <div className="flex justify-end gap-3">
+                  <Button variant="secondary" size="icon">
+                    <span className="icon-[hugeicons--pencil-edit-02]" />
+                  </Button>
+
+                  <Button variant="destructive" size="icon">
+                    <span className="icon-[hugeicons--delete-02]" />
+                  </Button>
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
       </TableBody>
 
       <TableFooter>
--- src/features/products/views/products-list.tsx
+++ src/features/products/views/products-list.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import ProductsTable from "../components/products-table";
 import { useProducts } from "../hooks/use-products";
@@ -9,6 +10,13 @@ function ProductsList() {
     <div className="p-4">
       <Card>
         <CardContent>
+          <div className="flex justify-end mb-3">
+            <Button>
+              <span className="icon-[hugeicons--plus]" />
+              <span>Add Product</span>
+            </Button>
+          </div>
+
           <ProductsTable
             products={products}
             isLoading={isFetching}
